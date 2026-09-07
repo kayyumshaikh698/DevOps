3950ddaa39e094f9c8f2cb42eda5a6089ed52f25
Kayyum <kayyumshaikh698@gmail.com>
2026-09-07 21:51:14 +0530

    added postgres,ansible and jenkins

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -3,21 +3,21 @@ terraform {
 }
 
 resource "terraform_data" "docker_compose" {
-  # Re-run when jenkins_compose.yml changes
+  # Re-run when postgres_ansible_jenkins_compose.yml changes
   triggers_replace = [
-    filemd5("${path.module}/jenkins_compose.yml")
+    filemd5("${path.module}/postgres_ansible_jenkins_compose.yml")
   ]
 
   # Start containers on apply
   provisioner "local-exec" {
-    command     = "docker compose -f jenkins_compose.yml up -d"
+    command     = "docker compose -f postgres_ansible_jenkins_compose.yml up -d"
     working_dir = path.module
   }
 
   # Stop and remove containers on destroy
   provisioner "local-exec" {
     when        = destroy
-    command     = "docker compose -f jenkins_compose.yml down"
+    command     = "docker compose -f postgres_ansible_jenkins_compose.yml down"
     working_dir = path.module
   }
 }
